b04f6f44db3bcf5997c22d2c6b03337c3fe86016
Steve Springett <steve@springett.us>
2017-10-14 16:50:01 -0500

    Codestyle corrections

--- sonar-dependency-check-plugin/src/main/resources/static/report.js
+++ sonar-dependency-check-plugin/src/main/resources/static/report.js
@@ -1,33 +1,33 @@
-window.registerExtension('dependencycheck/report', function(options) {
+window.registerExtension("dependencycheck/report", function(options) {
 	var isDisplayed = true;
 	
-	window.SonarRequest.getJSON('/api/measures/component', {
+	window.SonarRequest.getJSON("/api/measures/component", {
 		componentKey : options.component.key,
-		metricKeys : 'report'
+		metricKeys : "report"
 	}).then(function(response) {
 		if (isDisplayed) {
-			var htmlString = response.component.measures.filter(measure => measure.metric == 'report')[0].value;
+			var htmlString = response.component.measures.filter(measure => measure.metric === "report")[0].value;
 			var currentEl = options.el;
-			while (currentEl.id !== 'container') {
-				currentEl.style.display = 'flex';
-				currentEl.style.flex = '1 1 auto';
-				currentEl.style.flexDirection = 'column';
+			while (currentEl.id !== "container") {
+				currentEl.style.display = "flex";
+				currentEl.style.flex = "1 1 auto";
+				currentEl.style.flexDirection = "column";
 				currentEl = currentEl.parentElement;
 			}
-			currentEl.style.display = 'flex';
-			currentEl.style.flexDirection = 'column';
+			currentEl.style.display = "flex";
+			currentEl.style.flexDirection = "column";
 			
-			var reportFrame = document.createElement('iframe');
-			reportFrame.sandbox.value = 'allow-scripts allow-same-origin';
-			reportFrame.style.border = 'none';
-			reportFrame.style.flex= '1 1 auto';
+			var reportFrame = document.createElement("iframe");
+			reportFrame.sandbox.value = "allow-scripts allow-same-origin";
+			reportFrame.style.border = "none";
+			reportFrame.style.flex= "1 1 auto";
 			reportFrame.srcdoc = htmlString;
 			options.el.append(reportFrame);
 		}
 	});
 
 	return function() {
-		options.el.textContent = '';
+		options.el.textContent = "";
 		var isDisplayed = false;
 	};
 });
