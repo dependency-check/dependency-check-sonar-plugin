41aa8f8408e5b211e4661ad3a70553dc6be98329
Nick Harvey <Nick.Harvey.Developer@gmail.com>
2017-10-30 13:57:41 -0500

    fixing page styling issue

--- sonar-dependency-check-plugin/src/main/resources/static/report.js
+++ sonar-dependency-check-plugin/src/main/resources/static/report.js
@@ -1,21 +1,30 @@
 window.registerExtension('dependencycheck/report', function(options) {
 	var isDisplayed = true;
 	
+	if (!document.querySelector('style#dependency-check-report')) {
+		style = document.createElement("style");
+		style.id = 'dependency-check-report';
+		// WebKit hack :(
+		style.appendChild(document.createTextNode(""));
+		document.head.appendChild(style);
+		style.insertRule(".dependency-check-report-content {flex: 1 1 auto;}", 1);
+		style.insertRule(".dependency-check-report-container {display: flex; flex-direction: column;}", 2);
+	}
+	
 	window.SonarRequest.getJSON('/api/measures/component', {
 		componentKey : options.component.key,
 		metricKeys : 'report'
 	}).then(function(response) {
 		if (isDisplayed) {
 			var htmlString = response.component.measures.filter(measure => measure.metric == 'report')[0].value;
-			var currentEl = options.el;
+			options.el.classList.add('dependency-check-report-content');
+			var currentEl = options.el.parentElement;
 			while (currentEl.id !== 'container') {
-				currentEl.style.display = 'flex';
-				currentEl.style.flex = '1 1 auto';
-				currentEl.style.flexDirection = 'column';
+				currentEl.classList.add('dependency-check-report-content');
+				currentEl.classList.add('dependency-check-report-container');
 				currentEl = currentEl.parentElement;
 			}
-			currentEl.style.display = 'flex';
-			currentEl.style.flexDirection = 'column';
+			currentEl.classList.add('dependency-check-report-container');
 			
 			var reportFrame = document.createElement('iframe');
 			reportFrame.sandbox.value = 'allow-scripts allow-same-origin';
@@ -29,5 +38,13 @@ window.registerExtension('dependencycheck/report', function(options) {
 	return function() {
 		options.el.textContent = '';
 		var isDisplayed = false;
+		options.el.classList.remove('dependency-check-report-content');
+		var currentEl = options.el.parentElement;
+		while (currentEl.id !== 'container') {
+			currentEl.classList.remove('dependency-check-report-content');
+			currentEl.classList.remove('dependency-check-report-container');
+			currentEl = currentEl.parentElement;
+		}
+		currentEl.classList.remove('dependency-check-report-container');
 	};
 });
