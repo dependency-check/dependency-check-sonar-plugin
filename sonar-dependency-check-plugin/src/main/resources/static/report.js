4674e4181fc1e6e36e1405c97b5c3e0f8a4cc666
Nick Harvey <Nick.Harvey.Developer@gmail.com>
2017-10-30 14:41:23 -0500

    fixing page styling issue

--- sonar-dependency-check-plugin/src/main/resources/static/report.js
+++ sonar-dependency-check-plugin/src/main/resources/static/report.js
@@ -1,9 +1,9 @@
 window.registerExtension("dependencycheck/report", function(options) {
 	var isDisplayed = true;
 	
-	if (!document.querySelector('style#dependency-check-report')) {
+	if (!document.querySelector("style#dependency-check-report")) {
 		style = document.createElement("style");
-		style.id = 'dependency-check-report';
+		style.id = "dependency-check-report";
 		// WebKit hack :(
 		style.appendChild(document.createTextNode(""));
 		document.head.appendChild(style);
@@ -11,19 +11,19 @@ window.registerExtension("dependencycheck/report", function(options) {
 		style.sheet.insertRule(".dependency-check-report-container {display: flex; flex-direction: column;}", 0);
 	}
 	
-	window.SonarRequest.getJSON('/api/measures/component', {
+	window.SonarRequest.getJSON("/api/measures/component", {
 		componentKey : options.component.key,
 		metricKeys : "report"
 	}).then(function(response) {
 		if (isDisplayed) {
-			var htmlString = response.component.measures.filter(measure => measure.metric == 'report')[0].value;
+			var htmlString = response.component.measures.filter(measure => measure.metric === "report")[0].value;
 			var currentEl = options.el;
-			while (currentEl.id !== 'container') {
-				currentEl.classList.add('dependency-check-report-content');
-				currentEl.classList.add('dependency-check-report-container');
+			while (currentEl.id !== "container") {
+				currentEl.classList.add("dependency-check-report-content");
+				currentEl.classList.add("dependency-check-report-container");
 				currentEl = currentEl.parentElement;
 			}
-			currentEl.classList.add('dependency-check-report-container');
+			currentEl.classList.add("dependency-check-report-container");
 			
 			var reportFrame = document.createElement("iframe");
 			reportFrame.sandbox.value = "allow-scripts allow-same-origin";
@@ -38,11 +38,11 @@ window.registerExtension("dependencycheck/report", function(options) {
 		options.el.textContent = "";
 		var isDisplayed = false;
 		var currentEl = options.el;
-		while (currentEl.id !== 'container') {
-			currentEl.classList.remove('dependency-check-report-content');
-			currentEl.classList.remove('dependency-check-report-container');
+		while (currentEl.id !== "container") {
+			currentEl.classList.remove("dependency-check-report-content");
+			currentEl.classList.remove("dependency-check-report-container");
 			currentEl = currentEl.parentElement;
 		}
-		currentEl.classList.remove('dependency-check-report-container');
+		currentEl.classList.remove("dependency-check-report-container");
 	};
 });
