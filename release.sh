f69d9c512394ae7104fb0a3480c97ebc6cde1ad4
Steve Springett <steve@springett.us>
2018-08-17 12:38:23 -0500

    More release testing

--- release.sh
+++ release.sh
@@ -2,17 +2,18 @@
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 export PATH=JAVA_HOME/bin:$PATH
 
-read -p "Are you sure you want to release (Y/N)? "
+read -p "Are you sure you want to release? (Y/N)? "
 if ( [ "$REPLY" == "Y" ] ) then
-
-  #mvn release:rollback
+  read -p "Specify release version number to release (i.e. 1.0.0): "
   mvn clean
   mvn release:clean
 
-  #mvn github-release:release
-  #mvn release:prepare
-  #mvn release:perform -Prelease -X -e | tee release.log
+  mvn versions:set -DnewVersion=$REPLY
+  mvn github-release:release
+
+  mvn versions:revert
+  mvn release:clean release:prepare release:perform -Prelease -X -e | tee release.log
 
 else
-  echo -e "Exit without deploy"
+  echo -e "Exit without release"
 fi
