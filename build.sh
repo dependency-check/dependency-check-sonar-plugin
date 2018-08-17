28b91b2ffd14992195ba0209428e4b731ca93bb9
Steve Springett <steve@springett.us>
2018-08-17 11:25:24 -0500

    script updates

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+export PATH=JAVA_HOME/bin:$PATH
 
 mvn clean install $*
 
---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+export PATH=JAVA_HOME/bin:$PATH
+
+read -p "Are you sure you want to release (Y/N)? "
+if ( [ "$REPLY" == "Y" ] ) then
+
+  mvn clean
+  mvn release:clean release:prepare release:perform -Prelease -X -e | tee release.log
+
+else
+  echo -e "Exit without deploy"
+fi
