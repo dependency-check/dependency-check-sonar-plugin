92a8a0201270817379c5ef427d2526d4421c8955
thibaut severac <thib3113@gmail.com>
2019-05-03 22:04:41 +0200

    add assertions

--- sonar-dependency-check-plugin/src/test/java/org/sonar/dependencycheck/parser/ReportParserTest.java
+++ sonar-dependency-check-plugin/src/test/java/org/sonar/dependencycheck/parser/ReportParserTest.java
@@ -19,13 +19,8 @@
  */
 package org.sonar.dependencycheck.parser;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.io.InputStream;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 
 import org.junit.jupiter.api.Test;
 import org.sonar.dependencycheck.parser.element.Analysis;
@@ -34,6 +29,8 @@ import org.sonar.dependencycheck.parser.element.Dependency;
 import org.sonar.dependencycheck.parser.element.Evidence;
 import org.sonar.dependencycheck.parser.element.Identifier;
 import org.sonar.dependencycheck.parser.element.Vulnerability;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ReportParserTest {
 
@@ -209,6 +206,94 @@ public class ReportParserTest {
         assertEquals("project", analysis.getProjectInfo().get().getName());
         assertEquals("2019-04-23T22:43:06.450+0000", analysis.getProjectInfo().get().getReportDate());
         assertEquals("This report contains data retrieved from the National Vulnerability Database: https://nvd.nist.gov, NPM Public Advisories: https://www.npmjs.com/advisories, and the RetireJS community.", analysis.getProjectInfo().get().getCredits());
+
+        Collection<Dependency> dependencies = analysis.getDependencies();
+        assertEquals(3, dependencies.size());
+        Iterator<Dependency> iterator = dependencies.iterator();
+
+        // jquery
+        Dependency dependency = (Dependency) iterator.next();
+
+        assertEquals("jquery.js", dependency.getFileName());
+        assertEquals("project/node_modules/moment-duration-format/test/vendor/jquery.js", dependency.getFilePath());
+        assertEquals("cc41e74189d44a6169f56655e44ef69d", dependency.getMd5Hash());
+        assertEquals("e0e77771c69fe4acd4cad20a0f20ce7a5086dc56", dependency.getSha1Hash());
+
+        Collection<Evidence> evidenceCollected = dependency.getEvidenceCollected();
+        assertEquals(2, evidenceCollected.size());
+        for (Evidence evidence : evidenceCollected) {
+            assertFalse(evidence.getSource().isEmpty());
+            assertFalse(evidence.getName().isEmpty());
+            assertFalse(evidence.getValue().isEmpty());
+        }
+
+        Collection<Vulnerability> vulnerabilities = dependency.getVulnerabilities();
+        assertEquals(2, vulnerabilities.size());
+        Iterator<Vulnerability> vulnIterator = vulnerabilities.iterator();
+        Vulnerability vulnerability = (Vulnerability) vulnIterator.next();
+        assertEquals("CVE-2015-9251", vulnerability.getName());
+        assertEquals("NVD", vulnerability.getSource());
+        assertEquals(4.3f, vulnerability.getCvssScore(false), 0.0f);
+        assertEquals(6.1f, vulnerability.getCvssScore(), 0.0f);
+        assertEquals("MEDIUM", vulnerability.getSeverity());
+        assertEquals("MEDIUM", vulnerability.getSeverity(false));
+        assertFalse(vulnerability.getCwe().isPresent());
+        assertEquals("jQuery before 3.0.0 is vulnerable to Cross-site Scripting (XSS) attacks when a cross-domain Ajax request is performed without the dataType option, causing text/javascript responses to be executed.", vulnerability.getDescription());
+
+        vulnerability = (Vulnerability) vulnIterator.next();
+        assertEquals("CVE-2019-11358", vulnerability.getName());
+        assertEquals("NVD", vulnerability.getSource());
+        assertEquals(4.3f, vulnerability.getCvssScore(false), 0.0f);
+        assertEquals(6.1f , vulnerability.getCvssScore(), 0.0f);
+        assertEquals("MEDIUM", vulnerability.getSeverity());
+        assertFalse(vulnerability.getCwe().isPresent());
+        assertEquals("jQuery before 3.4.0, as used in Drupal, Backdrop CMS, and other products, mishandles jQuery.extend(true, {}, ...) because of Object.prototype pollution. If an unsanitized source object contained an enumerable __proto__ property, it could extend the native Object.prototype.", vulnerability.getDescription());
+
+        // kind-of -> no vulnerability
+        dependency = (Dependency) iterator.next();
+
+        assertEquals("index.js", dependency.getFileName());
+        assertEquals("project/node_modules/jest-config/node_modules/is-number/node_modules/kind-of/index.js", dependency.getFilePath());
+        assertEquals("974e0c2803e83c5bf65de52b98bf4f55", dependency.getMd5Hash());
+        assertEquals("f9e8418f23f97452410088786d5e0c7a981ced74", dependency.getSha1Hash());
+
+        evidenceCollected = dependency.getEvidenceCollected();
+        assertEquals(0, evidenceCollected.size());
+
+        vulnerabilities = dependency.getVulnerabilities();
+        assertEquals(0, vulnerabilities.size());
+
+
+        // braces -> vulnerability from NPM
+        dependency = (Dependency) iterator.next();
+
+        assertEquals("braces:1.8.5", dependency.getFileName());
+        assertEquals("project/node_modules/braces/package.json", dependency.getFilePath());
+        assertEquals("a5663cea473ad651ac4a00a504883a22", dependency.getMd5Hash());
+        assertEquals("8082af62f94236e20503e76f0c94b0cf38bf0824", dependency.getSha1Hash());
+
+        evidenceCollected = dependency.getEvidenceCollected();
+        assertEquals(6, evidenceCollected.size());
+        for (Evidence evidence : evidenceCollected) {
+            assertFalse(evidence.getSource().isEmpty());
+            assertFalse(evidence.getName().isEmpty());
+            assertFalse(evidence.getValue().isEmpty());
+        }
+
+        vulnerabilities = dependency.getVulnerabilities();
+        assertEquals(1, vulnerabilities.size());
+        vulnIterator = vulnerabilities.iterator();
+        vulnerability = (Vulnerability) vulnIterator.next();
+        assertEquals("786", vulnerability.getName());
+        assertEquals("NPM", vulnerability.getSource());
+        assertEquals(5.0f, vulnerability.getCvssScore(false), 0.0f);
+        assertEquals(5.0f, vulnerability.getCvssScore(), 0.0f);
+        assertEquals("MEDIUM", vulnerability.getSeverity());
+        assertEquals("MEDIUM", vulnerability.getSeverity(false));
+        assertFalse(vulnerability.getCwe().isPresent());
+        assertEquals("Versions of `braces` prior to 2.3.1 are vulnerable to Regular Expression Denial of\n" +
+                "                        Service (ReDoS). Untrusted input may cause catastrophic backtracking while matching regular\n" +
+                "                        expressions. This can cause the application to be unresponsive leading to Denial of Service.", vulnerability.getDescription());
     }
 
     @Test
@@ -219,6 +304,90 @@ public class ReportParserTest {
         assertEquals("project", analysis.getProjectInfo().get().getName());
         assertEquals("2019-04-23T22:43:06.450+0000", analysis.getProjectInfo().get().getReportDate());
         assertEquals("This report contains data retrieved from the National Vulnerability Database: https://nvd.nist.gov, NPM Public Advisories: https://www.npmjs.com/advisories, and the RetireJS community.", analysis.getProjectInfo().get().getCredits());
+
+        Collection<Dependency> dependencies = analysis.getDependencies();
+        assertEquals(10765, dependencies.size());
+        
+        for (Dependency dependency : dependencies) {
+            //check jquery
+            if ( "e0e77771c69fe4acd4cad20a0f20ce7a5086dc56".equals(dependency.getSha1Hash())){
+                assertEquals("jquery.js", dependency.getFileName());
+                assertEquals("project/node_modules/moment-duration-format/test/vendor/jquery.js", dependency.getFilePath());
+                assertEquals("cc41e74189d44a6169f56655e44ef69d", dependency.getMd5Hash());
+                assertEquals("e0e77771c69fe4acd4cad20a0f20ce7a5086dc56", dependency.getSha1Hash());
+
+                Collection<Evidence> evidenceCollected = dependency.getEvidenceCollected();
+                assertEquals(2, evidenceCollected.size());
+                for (Evidence evidence : evidenceCollected) {
+                    assertFalse(evidence.getSource().isEmpty());
+                    assertFalse(evidence.getName().isEmpty());
+                    assertFalse(evidence.getValue().isEmpty());
+                }
+
+                Collection<Vulnerability> vulnerabilities = dependency.getVulnerabilities();
+                assertEquals(2, vulnerabilities.size());
+                Iterator<Vulnerability> vulnIterator = vulnerabilities.iterator();
+                Vulnerability vulnerability = (Vulnerability) vulnIterator.next();
+                assertEquals("CVE-2015-9251", vulnerability.getName());
+                assertEquals("NVD", vulnerability.getSource());
+                assertEquals(4.3f, vulnerability.getCvssScore(false), 0.0f);
+                assertEquals(6.1f, vulnerability.getCvssScore(), 0.0f);
+                assertEquals("MEDIUM", vulnerability.getSeverity());
+                assertEquals("MEDIUM", vulnerability.getSeverity(false));
+                assertFalse(vulnerability.getCwe().isPresent());
+                assertEquals("jQuery before 3.0.0 is vulnerable to Cross-site Scripting (XSS) attacks when a cross-domain Ajax request is performed without the dataType option, causing text/javascript responses to be executed.", vulnerability.getDescription());
+
+                vulnerability = (Vulnerability) vulnIterator.next();
+                assertEquals("CVE-2019-11358", vulnerability.getName());
+                assertEquals("NVD", vulnerability.getSource());
+                assertEquals(4.3f, vulnerability.getCvssScore(false), 0.0f);
+                assertEquals(6.1f , vulnerability.getCvssScore(), 0.0f);
+                assertEquals("MEDIUM", vulnerability.getSeverity());
+                assertFalse(vulnerability.getCwe().isPresent());
+                assertEquals("jQuery before 3.4.0, as used in Drupal, Backdrop CMS, and other products, mishandles jQuery.extend(true, {}, ...) because of Object.prototype pollution. If an unsanitized source object contained an enumerable __proto__ property, it could extend the native Object.prototype.", vulnerability.getDescription());
+            }
+            //check kind-of
+            if ( "f9e8418f23f97452410088786d5e0c7a981ced74".equals(dependency.getSha1Hash())){
+                assertEquals("index.js", dependency.getFileName());
+                assertEquals("project/node_modules/jest-config/node_modules/is-number/node_modules/kind-of/index.js", dependency.getFilePath());
+                assertEquals("974e0c2803e83c5bf65de52b98bf4f55", dependency.getMd5Hash());
+                assertEquals("f9e8418f23f97452410088786d5e0c7a981ced74", dependency.getSha1Hash());
+
+                Collection<Evidence> evidenceCollected = dependency.getEvidenceCollected();
+                assertEquals(0, evidenceCollected.size());
+
+                Collection<Vulnerability> vulnerabilities = dependency.getVulnerabilities();
+                assertEquals(0, vulnerabilities.size());
+            }
+            //check brace:1.8.5 => NPM
+            if ("8082af62f94236e20503e76f0c94b0cf38bf0824".equals(dependency.getSha1Hash())){
+                assertEquals("braces:1.8.5", dependency.getFileName());
+                assertEquals("project/node_modules/braces/package.json", dependency.getFilePath());
+                assertEquals("a5663cea473ad651ac4a00a504883a22", dependency.getMd5Hash());
+                assertEquals("8082af62f94236e20503e76f0c94b0cf38bf0824", dependency.getSha1Hash());
+
+                Collection<Evidence> evidenceCollected = dependency.getEvidenceCollected();
+                assertEquals(6, evidenceCollected.size());
+                for (Evidence evidence : evidenceCollected) {
+                    assertFalse(evidence.getSource().isEmpty());
+                    assertFalse(evidence.getName().isEmpty());
+                    assertFalse(evidence.getValue().isEmpty());
+                }
+
+                Collection<Vulnerability> vulnerabilities = dependency.getVulnerabilities();
+                assertEquals(1, vulnerabilities.size());
+                Iterator<Vulnerability> vulnIterator = vulnerabilities.iterator();
+                Vulnerability vulnerability = (Vulnerability) vulnIterator.next();;
+                assertEquals("786", vulnerability.getName());
+                assertEquals("NPM", vulnerability.getSource());
+                assertEquals(5.0f, vulnerability.getCvssScore(false), 0.0f);
+                assertEquals(5.0f, vulnerability.getCvssScore(), 0.0f);
+                assertEquals("MEDIUM", vulnerability.getSeverity());
+                assertEquals("MEDIUM", vulnerability.getSeverity(false));
+                assertFalse(vulnerability.getCwe().isPresent());
+                assertEquals("Versions of `braces` prior to 2.3.1 are vulnerable to Regular Expression Denial of Service (ReDoS). Untrusted input may cause catastrophic backtracking while matching regular expressions. This can cause the application to be unresponsive leading to Denial of Service.", vulnerability.getDescription());
+            }
+        }
     }
 
 }
