783a080f1e4895ec0ffdc6a2449ed9f8797a12fe
Nikita Sumahers <36233163+nikitashum@users.noreply.github.com>
2022-10-05 13:09:48 +0200

    Update XMLReportParserHelperTest.java
    Fix tests after jackson upgrade

--- sonar-dependency-check-plugin/src/test/java/org/sonar/dependencycheck/parser/XMLReportParserHelperTest.java
+++ sonar-dependency-check-plugin/src/test/java/org/sonar/dependencycheck/parser/XMLReportParserHelperTest.java
@@ -160,7 +160,6 @@ class XMLReportParserHelperTest extends ReportParserTest {
         assertEquals(4.0f, vulnerability.getCvssScore(context.config()), 0.0f);
         assertEquals("MEDIUM", vulnerability.getSeverity());
         assertEquals("MEDIUM", vulnerability.getSeverity(false));
-        assertFalse(vulnerability.getCwes().isPresent());
         assertEquals("Versions of `braces` prior to 2.3.1 are vulnerable to Regular Expression Denial of\n" +
             "                        Service (ReDoS). Untrusted input may cause catastrophic backtracking while matching regular\n" +
             "                        expressions. This can cause the application to be unresponsive leading to Denial of Service.", vulnerability.getDescription());
@@ -260,7 +259,6 @@ class XMLReportParserHelperTest extends ReportParserTest {
                 assertEquals(4.0f, vulnerability.getCvssScore(context.config()), 0.0f);
                 assertEquals("MEDIUM", vulnerability.getSeverity());
                 assertEquals("MEDIUM", vulnerability.getSeverity(false));
-                assertFalse(vulnerability.getCwes().isPresent());
                 assertEquals(
                     "Versions of `braces` prior to 2.3.1 are vulnerable to Regular Expression Denial of Service (ReDoS). Untrusted input may cause catastrophic backtracking while matching regular expressions. This can cause the application to be unresponsive leading to Denial of Service.",
                     vulnerability.getDescription());
